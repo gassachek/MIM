e624e152d3b59490fb7ec402937c214781b9eb0b
Gassag <83364617+CameronKing18@users.noreply.github.com>
2023-05-16 23:38:52 +0300

    Add files via upload

---
+++ site/main.js
@@ -0,0 +1,7 @@
+$('.menu-btn').on('click', function(e) {
+    e.preventDefault();
+    $('.header').toggleClass('menu_active');
+    $('.content').toggleClass('content_active');
+    $('.header-menu__icon').toggleClass('icon_active');
+    $('.menu-btn').toggleClass('menu-btn_active');
+})
