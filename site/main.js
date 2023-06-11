593a8dbffc97bd1a527765d48a91ffc403699d80
Gassag <83364617+CameronKing18@users.noreply.github.com>
2023-06-11 20:35:10 +0300

    Update main.js

--- site/main.js
+++ site/main.js
@@ -1,8 +1,6 @@
 $('.menu-btn').on('click', function(e) {
     e.preventDefault();
     $('.header').toggleClass('menu_active');
-    $('.content').toggleClass('content_active');
     $('.header-menu__icon').toggleClass('icon_active');
     $('.menu-btn').toggleClass('menu-btn_active');
-    $('.content-menu__item').toggleClass('.content-menu__item_active');
 })
