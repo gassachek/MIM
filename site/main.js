5c53cbff2bcde4c4270af0f98b687beb62cd9802
Gassag <83364617+CameronKing18@users.noreply.github.com>
2023-06-12 17:17:58 +0300

    Delete main.js

--- site/main.js
+++
@@ -1,6 +0,0 @@
-$('.menu-btn').on('click', function(e) {
-    e.preventDefault();
-    $('.header').toggleClass('menu_active');
-    $('.header-menu__icon').toggleClass('icon_active');
-    $('.menu-btn').toggleClass('menu-btn_active');
-})
