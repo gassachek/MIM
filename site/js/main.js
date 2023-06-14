4b8e5cdb4ca9e1ac32af8f6fce817f9b89fc98f3
Gassag <83364617+CameronKing18@users.noreply.github.com>
2023-06-14 12:54:16 +0300

    Add files via upload

---
+++ site/js/main.js
@@ -0,0 +1,41 @@
+document.querySelector('.header-menu--btn').addEventListener('click', function() {
+    document.querySelectorAll('[class^="header"]').forEach(function(element) {
+        if (element.classList.contains('active')) {
+            element.classList.remove('active');
+        } else {
+            element.classList.add('active');
+        }
+    });
+});
+
+document.addEventListener('click', function(event) {
+    var headerElements = document.querySelectorAll('[class^="header"]');
+    var isClickedInsideHeader = false;
+
+    headerElements.forEach(function(element) {
+        if (element.contains(event.target)) {
+            isClickedInsideHeader = true;
+        }
+    });
+
+    if (!isClickedInsideHeader) {
+        headerElements.forEach(function(element) {
+            element.classList.remove('active');
+        });
+    }
+});
+
+
+
+
+// $('.header-menu-btn').click(function() {
+//     $('[class^="header"]').filter(function() {
+//         return this.className.match(/\bheader/);
+//     }).toggleClass('active');
+// });
+
+// $(document).on('click', function(event) {
+//     if (!$(event.target).closest('.header').length) {
+//         $('[class^="header"]').removeClass('active');
+//     }
+// });
